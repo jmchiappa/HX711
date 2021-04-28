ff5d74b30f70f0d31fb1e7aa83b7d7c9690ebc89
Jean-Marc <null>
2021-04-28 14:59:04 +0200

    update basic example

--- examples/HX711_basic_example/HX711_basic_example.ino
+++ examples/HX711_basic_example/HX711_basic_example.ino
@@ -4,23 +4,31 @@
 const int LOADCELL_DOUT_PIN = 2;
 const int LOADCELL_SCK_PIN = 3;
 
-HX711 scale;
+HX711 cell;
+
+boolean complete = false;
+
+void displayValue(float weight) {
+  Serial.println(weight);
+  complete = true;
+}
 
 void setup() {
+
   Serial.begin(57600);
-  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+
+  cell.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  cell.set_scale(2280.f);                      // this value is obtained by calibrating the scale with known weights; see the README for details  
+  cell.tare();
+
+  complete = true; // start a new cycle
 }
 
 void loop() {
 
-  if (scale.is_ready()) {
-    long reading = scale.read();
-    Serial.print("HX711 reading: ");
-    Serial.println(reading);
-  } else {
-    Serial.println("HX711 not found.");
+  if (complete) {
+    complete=false;
+    Serial.print("nouvelle valeur : ");
+    cell.get_units(displayValue,50);
   }
-
-  delay(1000);
-  
 }
